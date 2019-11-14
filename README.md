dcfc5c3363130077466d1ab55767733f36025c54
rgmyr <ross.meyer@utexas.edu>
2019-11-14 01:56:25 -0700

    fix tensor shapes for difference op; README adds

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # TensorFlow-ProSeNet
 
+This is a `tf.keras` implementation of [Interpretable and Steerable Sequence Learning via Prototypes](https://arxiv.org/abs/1907.09728). Whether I'll implement the "steering" part remains to be seen -- probably not worth it for my purposes.
+
 ## Notes
 
 The complete loss minimized is a combination of:
@@ -14,4 +16,4 @@ Additional required operations:
 
 - **Prototype projection**: Every few (~4) epochs, prototype vectors are re-assigned to the closest embedding sequence in the training set.
 - **Prototype interpretation**: Need to method to grab classification weights assigned to prototype and interpret their associated label(s).
-- **Prototype simplification**: Rather than projecting prototypes to complete sequences, use beam search to find a subsequence containing critical events
+- **Prototype simplification**: Rather than projecting prototypes to complete sequences, use beam search to find a subsequence containing critical events. **NOTE**: the paper is ambiguous as to whether this is used during training (as the "projection" step) or whether this is just an interpretation tool.
--- prosenet/prototype.py
+++ prosenet/prototype.py
@@ -1,37 +1,62 @@
 """
-A `Prototype` Layer and related operations.
+A `Prototypes` Layer and related operations.
 """
 import tensorflow as tf
 from tensorflow.keras.layers import Layer as KerasLayer
 
-class Prototype(KerasLayer):
+
+class Prototypes(KerasLayer):
     """
-    Should we combine this with a (positive restricted) Linear classifier, or handle that in a full model?
+    Should we combine this with a (positive restricted) Linear classifier,
+    or handle that in a full model?
+
+    Do we add losses in `call`?
     """
-    def __init__(self, k, **kwargs):
+    def __init__(self, k, dmin=1.0, **kwargs):
         """
         Parameters
         ----------
         k : int
             Number of prototype vectors to create.
+        dmin : float, optional
+            Threshold to determine whether to prototypes are close, default=1.0.
+            For "diversity" regularization. See paper section 3.2 for details.
         """
         super(Prototype, self).__init__(**kwargs)
         self.k = k
+        self.dmin = dmin
+
 
     def build(self, input_shape):
 
-        # what initializer should we use?
         self.prototypes = self.add_weight(
             name='prototypes',
-            shape=(self.k, input_shape[-1]),
-            initializer='random_normal',
+            shape=(1, self.k, input_shape[-1]),
+            initializer='random_normal',        # is this the right init?
             trainable=True
         )
+
 
     def call(self, x):
 
         # L2 distances from prototypes
+        x = tf.expand_dims(x, -2)
         d2 = tf.norm(x - self.prototypes, ord=2, axis=-1)
 
-        # return exponentially squashed
         return tf.exp(-d2)
+
+
+    def _diversity_loss(self):
+        # Compute the "diversity" loss,
+        # which penalizes prototypes that are close to each other
+        pass
+
+    def _clustering_loss(self, d2):
+        # Compute the "clustering" loss,
+        # which minimizes distance between encodings and nearest prototypes
+        pass
+
+    def _evidence_loss(self, d2):
+        # Compute the "evidence" loss,
+        # which pushes each prototype to be close to an encoding
+        pass
